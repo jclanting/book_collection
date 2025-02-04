b72b12c408240380f759dc067f2b840c7f45c740
root <root@localhost>
2025-02-04 02:23:38 +0000

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create!([
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", price: 10.99, published_date: "1925-04-10" },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", price: 8.99, published_date: "1960-07-11" },
+  { title: "1984", author: "George Orwell", price: 9.99, published_date: "1949-06-08" },
+  { title: "Pride and Prejudice", author: "Jane Austen", price: 7.99, published_date: "1813-01-28" },
+  { title: "The Catcher in the Rye", author: "J.D. Salinger", price: 11.50, published_date: "1951-07-16" }
+])
