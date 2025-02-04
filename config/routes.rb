fa29230106a5efa2bdfa044dc681ef830f055ee5
root <root@localhost>
2025-02-04 02:13:28 +0000

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  root 'books#indexs'
+  root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
